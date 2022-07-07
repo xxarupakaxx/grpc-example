c83bc98413736213741c185ee2d3ba83703e6d25
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-07-07 15:39:13 +0900

    add grpc server

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	"github.com/sirupsen/logrus"
+	"github.com/xxarupakaxx/grpc-example/gen/pb"
+	"google.golang.org/genproto/googleapis/cloud/osconfig/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	lis,err := net.Listen("tcp",":50051")
+	if err != nil {
+		log.Fatalf("failed to listen : %w",err)
+	}
+
+	logusLogger := logrus.New()
+	logrusEnty := logrus.NewEntry(logusLogger)
+	grpc_logrus.ReplaceGrpcLogger(logrusEnty)
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_logrus.UnaryServerInterceptor(logrusEnty)))
+
+	pb.RegisterMatchingServiceServer()
+
+	reflection.Register(server)
+
+	go func() {
+		log.Println("start grpc server ")
+
+		if err = server.Serve(lis);err!=nil {
+			log.Println("failed to start server")
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping grpc server")
+	server.GracefulStop()
+}
